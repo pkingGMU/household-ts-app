c2532499e11c477de6e23f192d05c92931078a8c
pkingGMU <pking23@gmu.edu>
2025-08-26 19:17:55 -0400

    Learned Routes to setup other pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,34 @@
 import "./App.css";
 import TaskManager from "./components/TaskManager.tsx";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
-function App() {
+function HomePage() {
+  return <h1>Home Page</h1>;
+}
+
+function TaskManagerPage() {
   return (
     <>
       <h1>BakuDeku Club</h1>
       <TaskManager />
+    </>
+  );
+}
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/TaskManagerPage">Task Manager</Link>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/TaskManagerPage" element={<TaskManagerPage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
